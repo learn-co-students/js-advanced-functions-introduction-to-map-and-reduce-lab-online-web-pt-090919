bb8bcd841574f200645f26667cd7dac851110948
fasnimo <fasnimo@gmail.com>
2020-04-30 21:33:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+let mapToNegativize = (sourceArray) => {
+        let thisNum = -1
+        return sourceArray.map(num => num * thisNum)
+    // for (let i = 0; i < sourceArray.length; i++) {
+    //     if (sourceArray[i] <= 1){
+    //         return sourceArray[i] * thisNum
+    //     }
+    // }
+} 
+
+const mapToNoChange = (dune) => {
+    const iterator = dune.map(function(name){
+        return name
+    })      
+}
+
+// for (const value of iterator) {
+//     return value
+// }
+    // let arr = sourceArray.map(function(name) {
+    //     let arr2 = []
+    //     for (let i = 0; i < sourceArray.length; i++) {
+    //         return arr2.push(name[i] + 1)
+    //     }
+    // })
+    // return arr
