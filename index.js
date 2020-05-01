09b8405c5fb6a7d7d8a453828ddf543022e4d89f
fasnimo <fasnimo@gmail.com>
2020-04-30 23:05:13 -0500

    Done.

--- index.js
+++ index.js
@@ -10,18 +10,54 @@ let mapToNegativize = (sourceArray) => {
 } 
 
 const mapToNoChange = (sourceArray) => {
-    const iterator = sourceArray.map(function(name){
-        return name
-    })      
+    const newArr = []
+    for(let i = 0; i < sourceArray.length; i++) {
+	     newArr.push(sourceArray[i])
+    }   
+    return newArr  
 }
 
-// for (const value of iterator) {
-//     return value
-// }
-    // let arr = sourceArray.map(function(name) {
-    //     let arr2 = []
-    //     for (let i = 0; i < sourceArray.length; i++) {
-    //         return arr2.push(name[i] + 1)
-    //     }
-    // })
-    // return arr
+const mapToDouble = (sourceArray) => {
+    const newArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i] * 2)
+    }
+    return newArr
+}
+
+const mapToSquare = (sourceArray) => {
+    const newArr = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i] ** 2)
+    }
+    return newArr
+}
+
+
+const reduceToTotal = (sourceArray, start = 0) => {
+    let reducer = start
+     for(let num of sourceArray){
+         reducer = reducer + num
+     }
+     return reducer
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    let toTure = true
+    for(let values of sourceArray) {
+        if(values == false) {
+            toTure = false
+        }
+    }
+    return toTure 
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    let toFalse = false
+    for(let value of sourceArray) {
+        if (value === true) {
+            toFalse = true
+        }
+    }    
+    return toFalse
+}
